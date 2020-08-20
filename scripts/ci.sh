a52cd836cfbe0c4aecc2e31e9317c51dffbebbe9
Petr Janik <485122@mail.muni.cz>
2020-08-20 09:56:23 +0200

    Add ci.sh script

---
+++ scripts/ci.sh
@@ -0,0 +1,13 @@
+build_test_clippy(){
+  while read data; do
+    TOML="$data"/Cargo.toml
+    printf "%s\n" "$TOML"
+    cargo build --verbose --manifest-path "$TOML"
+    cargo test --verbose --manifest-path "$TOML"
+    cargo clippy --verbose --manifest-path "$TOML"
+  done
+}
+
+find . -name 'Cargo.toml' -printf '%h\n' | sort -u | build_test_clippy
+
+
