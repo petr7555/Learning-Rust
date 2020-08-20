8a6cafcdfbc283582d64744d0ad26d32429c7b28
Petr Janik <485122@mail.muni.cz>
2020-08-20 10:20:26 +0200

    Simplify ci.sh

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,13 +1,12 @@
 build_test_clippy(){
-  while read data; do
-    TOML="$data"/Cargo.toml
-    printf "Project: %s\n" "$TOML"
-    cargo build --verbose --manifest-path "$TOML"
-    cargo test --verbose --manifest-path "$TOML"
-    cargo clippy --verbose --manifest-path "$TOML"
+  while read path; do
+    printf "Project: %s\n" "$path"
+    cargo build --verbose --manifest-path "$path"
+    cargo test --verbose --manifest-path "$path"
+    cargo clippy --verbose --manifest-path "$path"
   done
 }
 
-find . -name 'Cargo.toml' -printf '%h\n' | sort -u | build_test_clippy
+find . -name 'Cargo.toml' | sort -u | build_test_clippy
 
 
