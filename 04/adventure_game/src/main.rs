5ed11aab016071811cf3b3207f87affb6945e2b4
Petr Janik <485122@mail.muni.cz>
2020-08-26 15:36:10 +0200

    Finish implementation of 04 adventure game

--- 04/adventure_game/src/main.rs
+++ 04/adventure_game/src/main.rs
@@ -1,95 +1,159 @@
 use std::{env, io};
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::{BufRead, BufReader, Error};
+use std::io::{Error, Read};
 
-type Choice = (String, u32);
+use rand::Rng;
+use serde::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize)]
+struct Scenes {
+    scenes: Vec<Scene>
+}
+
+#[derive(Serialize, Deserialize, Clone)]
 struct Scene {
+    id: u32,
+    fight_scene_info: Option<FightSceneInfo>,
     story: String,
     choices: Vec<Choice>,
 }
 
-impl Scene {
-    fn from(story: String, choices: Vec<Choice>) -> Scene {
-        Scene {
-            story,
-            choices,
+#[derive(Serialize, Deserialize, Clone)]
+struct FightSceneInfo {
+    enemy: Character,
+    player: Character,
+    on_death: u32,
+    on_win: u32,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+struct Character {
+    health: i32,
+    attack_min: i32,
+    attack_max: i32,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+struct Choice {
+    text: String,
+    next_scene_id: u32,
+}
+
+trait Executable {
+    /// Takes current scene as `self` and map of all scenes, returns next scene.
+    fn execute(self, scenes: &HashMap<u32, Scene>) -> Scene;
+}
+
+impl Executable for Scene {
+    fn execute(self, scenes: &HashMap<u32, Scene>) -> Scene {
+        print_scene_story(&self);
+        print_scene_choices(&self);
+        if self.fight_scene_info.is_none() {
+            let choice = get_choice_from_user(self.choices.len() as u32);
+            let next_scene_id: u32 = self.choices[choice as usize - 1].next_scene_id;
+            return scenes[&next_scene_id].clone();
         }
+        let mut info = self.fight_scene_info.unwrap();
+        if info.player.health <= 0 {
+            return scenes[&info.on_death].clone();
+        }
+        if info.enemy.health <= 0 {
+            return scenes[&info.on_win].clone();
+        }
+        println!("Your health: {}; attack damage range: {}-{}", info.player.health, info.player.attack_min, info.player.attack_max);
+        println!("Enemy health: {}; attack damage range: {}-{}", info.enemy.health, info.enemy.attack_min, info.enemy.attack_max);
+        let choice = get_choice_from_user(self.choices.len() as u32);
+        // choice 1 is always attack
+        if choice == 1 {
+            let mut rng = rand::thread_rng();
+            let player_dmg = rng.gen_range(info.player.attack_min, info.player.attack_max);
+            let enemy_dmg = rng.gen_range(info.enemy.attack_min, info.enemy.attack_max);
+            println!("Player attacks and deals {} damage.", player_dmg);
+            println!("Enemy attacks at the same time and deals {} damage.", enemy_dmg);
+            info.enemy.health -= player_dmg;
+            info.player.health -= enemy_dmg;
+            return Scene {
+                id: self.id,
+                fight_scene_info: Option::from(info),
+                story: self.story,
+                choices: self.choices,
+            };
+        };
+        let next_scene_id: u32 = self.choices[choice as usize - 1].next_scene_id;
+        return scenes[&next_scene_id].clone();
     }
 }
 
 fn main() -> std::io::Result<()> {
     let args = get_args();
 
-    let scenes = parse_scenes(&args)?;
+    let scenes = parse_scenes(&args[1])?;
+    let scenes_map = scenes_into_map(scenes);
 
-    play(&scenes)?;
+    play(scenes_map)?;
 
     Ok(())
 }
 
-fn play(scenes: &HashMap<u32, Scene>) -> Result<(), Error> {
+fn scenes_into_map(scenes: Scenes) -> HashMap<u32, Scene> {
+    let scenes_vec = scenes.scenes;
+    let mut scenes_map = HashMap::new();
+    for scene in scenes_vec {
+        scenes_map.insert(scene.id, scene);
+    }
+    scenes_map
+}
+
+fn play(scenes: HashMap<u32, Scene>) -> Result<(), Error> {
     const INITIAL_SCENE_INDEX: u32 = 0;
 
-    let mut current_scene: &Scene = &scenes[&INITIAL_SCENE_INDEX];
-    while !current_scene.choices.is_empty() {
-        println!("{}", current_scene.story);
-        for (choice, _) in &current_scene.choices {
-            println!("{}", choice);
-        }
-        let choice = get_choice_from_user(current_scene.choices.len() as u32)?;
-        let next_scene: u32 = current_scene.choices[choice as usize - 1].1;
-        current_scene = &scenes[&next_scene];
+    let mut current_scene: Scene = scenes.get(&INITIAL_SCENE_INDEX)
+        .expect(format!("Story file must contain scene with id {}", INITIAL_SCENE_INDEX).as_str()).clone();
+    while !is_end_scene(&current_scene) {
+        current_scene = current_scene.execute(&scenes);
     }
-    println!("{}", current_scene.story);
+    print_scene_story(&current_scene);
     Ok(())
 }
 
-pub fn get_choice_from_user(num_of_choices: u32) -> Result<u32, Error> {
+fn is_end_scene(current_scene: &Scene) -> bool {
+    current_scene.choices.is_empty()
+}
+
+fn print_scene_story(current_scene: &Scene) {
+    println!("\n{}", current_scene.story);
+}
+
+fn print_scene_choices(current_scene: &Scene) {
+    for choice in &current_scene.choices {
+        println!("{}", choice.text);
+    }
+}
+
+fn get_choice_from_user(num_of_choices: u32) -> u32 {
     loop {
         println!("What do you choose to do?");
         let mut choice = String::new();
 
-        io::stdin().read_line(&mut choice)?;
+        io::stdin().read_line(&mut choice).expect("Cannot read user input.");
         match choice.trim().parse::<u32>() {
             Ok(val) => {
                 if val < 1 || val > num_of_choices {
                     continue;
                 }
-                break Ok(val);
+                break val;
             }
             Err(_) => continue
         };
     }
 }
 
-fn parse_scenes(arguments: &[String]) -> Result<HashMap<u32, Scene>, Error> {
-    let input_file = File::open(&arguments[1])?;
-    let input_reader = BufReader::new(input_file);
-
-    let mut scenes = HashMap::new();
-
-    let mut lines: Vec<String> = Vec::new();
-    for line in input_reader.lines() {
-        let current_line = line?;
-
-        if current_line.is_empty() {
-            let id: u32 = lines[0].parse().unwrap();
-            let story = String::from(&lines[1]);
-            let choices = lines.iter().skip(2).map(|line| {
-                let choice_and_next_scene: Vec<&str> = line.split(';').collect();
-                (String::from(choice_and_next_scene[0]), choice_and_next_scene[1].trim().parse().unwrap())
-            }).collect();
-            let scene = Scene::from(
-                story,
-                choices);
-            scenes.insert(id, scene);
-            lines.clear();
-        } else {
-            lines.push(current_line);
-        }
-    }
+fn parse_scenes(story_file: &str) -> Result<Scenes, Error> {
+    let mut input_file = File::open(story_file)?;
+    let mut data = String::new();
+    input_file.read_to_string(&mut data)?;
+    let scenes: Scenes = serde_json::from_str(&*data)?;
     Ok(scenes)
 }
 
