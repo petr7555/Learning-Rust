41ffdf3bff3b38912ce4264ec4e04f9d80fa0422
Petr Janik <485122@mail.muni.cz>
2020-09-01 08:50:52 +0200

    Dining philosophers with channel

--- 05/dining_philosophers/src/main.rs
+++ 05/dining_philosophers/src/main.rs
@@ -1,4 +1,4 @@
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, mpsc, Mutex};
 use std::thread;
 use std::time::Duration;
 
@@ -13,9 +13,11 @@ fn main() {
         sticks.push(Mutex::new(0));
     }
     let sticks = Arc::new(sticks);
+    let (sender, receiver) = mpsc::channel();
 
     for philosopher_num in 0..NUM_OF_PHILOSOPHERS {
         let sticks = Arc::clone(&sticks);
+        let sender = sender.clone();
         handles.push(thread::spawn(move || {
             for _ in 0..10 {
                 {
@@ -23,23 +25,29 @@ fn main() {
                     let mut right_stick = sticks[(philosopher_num + 1) % 5].lock().unwrap();
 
                     // eating
-                    println!("Philosopher {} started eating", philosopher_num);
+                    sender.send(format!("Philosopher {} started eating", philosopher_num)).unwrap();
                     thread::sleep(Duration::from_millis(rand::thread_rng().gen_range(20, 50)));
-                    println!("Philosopher {} stopped eating", philosopher_num);
+                    sender.send(format!("Philosopher {} stopped eating", philosopher_num)).unwrap();
 
                     *left_stick += 1;
                     *right_stick += 1;
                 }
 
                 // thinking
-                println!("Philosopher {} started thinking", philosopher_num);
+                sender.send(format!("Philosopher {} started thinking", philosopher_num)).unwrap();
                 thread::sleep(Duration::from_millis(rand::thread_rng().gen_range(20, 50)));
-                println!("Philosopher {} stopped thinking", philosopher_num);
+                sender.send(format!("Philosopher {} stopped thinking", philosopher_num)).unwrap();
             }
         }));
     }
+    // closes sender, so receiver does not expect more messages
+    drop(sender);
 
     handles.into_iter().for_each(|handle| { handle.join().unwrap(); });
+    receiver.iter().for_each(|msg| {
+        println!("{}", msg);
+    });
+
     sticks.iter().for_each(|stick| {
         println!("Stick was used {} times", stick.lock().unwrap());
     });
